2a057c7fe070326a32c11d278e7818c8d0b601e6
Ivan Pesenti <ipesenti@sorint.com>
2023-11-10 10:14:28 +0100

    09-gin-dto

--- handlers/bookings_add.go
+++ handlers/bookings_add.go
@@ -6,6 +6,16 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type BookingDTO struct {
+	RoomId   string `json:"room_id" binding:"required"`
+	BookedOn string `json:"booked_on" binding:"required"`
+}
+
 func AddBooking(c *gin.Context) {
-	c.String(http.StatusOK, "AddBooking()")
+	var bookingDto BookingDTO
+	if err := c.ShouldBind(&bookingDto); err != nil {
+		c.String(http.StatusBadRequest, err.Error())
+		return
+	}
+	c.JSON(http.StatusOK, bookingDto)
 }
