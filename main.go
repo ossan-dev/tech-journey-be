3db73c7003cfe1e48be0929f11a302bd858d4289
Ivan Pesenti <ipesenti@sorint.com>
2023-11-13 12:36:18 +0100

    22-middlewares

--- main.go
+++ main.go
@@ -50,10 +50,10 @@ func main() {
 	r.GET("/rooms", handlers.GetAllRooms)
 	r.GET("/rooms/:id", handlers.GetRoomById)
 	r.GET("/rooms/:id/photos", handlers.GetRoomPhotos)
-	r.GET("/bookings", handlers.GetBookingsByUserId)
-	r.GET("/bookings/:id", handlers.GetBookingById)
-	r.POST("/bookings", handlers.AddBooking)
-	r.DELETE("/bookings/:id", handlers.DeleteBooking)
+	r.GET("/bookings", middlewares.AuthorizeUser(), handlers.GetBookingsByUserId)
+	r.GET("/bookings/:id", middlewares.AuthorizeUser(), handlers.GetBookingById)
+	r.POST("/bookings", middlewares.AuthorizeUser(), handlers.AddBooking)
+	r.DELETE("/bookings/:id", middlewares.AuthorizeUser(), handlers.DeleteBooking)
 
 	if err := r.Run(":8080"); err != nil {
 		panic(err)
---
+++ middlewares/auth.go
@@ -0,0 +1,37 @@
+package middlewares
+
+import (
+	"net/http"
+
+	"coworkingapp/models"
+	"coworkingapp/utils"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+func AuthorizeUser() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		tokenHeader := c.GetHeader("Authorization")
+		if tokenHeader == "" {
+			c.JSON(http.StatusUnauthorized, models.CoworkingErr{Code: models.MissingTokenErr, Message: "please provide a jwt token along with the HTTP headers"})
+			return
+		}
+		secretKey := c.MustGet("ConfigKey").(models.CoworkingConfig).SecretKey
+		claims, err := utils.ValidateToken(tokenHeader, []byte(secretKey))
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, models.CoworkingErr{Code: models.TokenNotValidErr, Message: err.Error()})
+			return
+		}
+		email := (*claims)["sub"].(string)
+		db := c.MustGet("DbKey").(*gorm.DB)
+		user, err := models.GetUserByEmail(db, email)
+		if err != nil {
+			coworkingErr := err.(models.CoworkingErr)
+			c.JSON(coworkingErr.StatusCode, coworkingErr)
+			return
+		}
+		c.Set("UserIdKey", user.ID)
+		c.Next()
+	}
+}
