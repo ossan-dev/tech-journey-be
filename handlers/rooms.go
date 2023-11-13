04bcef88d22b78d2da9ad768e612dde6ecff0054
Ivan Pesenti <ipesenti@sorint.com>
2023-11-13 15:08:34 +0100

    23-rooms

--- handlers/rooms.go
+++ handlers/rooms.go
@@ -2,18 +2,53 @@ package handlers
 
 import (
 	"net/http"
+	"time"
+
+	"coworkingapp/models"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
 func GetAllRooms(c *gin.Context) {
-	c.String(http.StatusOK, "GetAllRooms()")
+	var dayToBook time.Time
+	var err error
+	rawDayToBook := c.Query("day_to_book")
+	if rawDayToBook != "" {
+		dayToBook, err = time.Parse("2006-01-02", rawDayToBook)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, models.CoworkingErr{Code: models.DateWrongFormatErr, Message: err.Error()})
+			return
+		}
+	}
+	db := c.MustGet("DbKey").(*gorm.DB)
+	rooms, err := models.GetRooms(db, dayToBook)
+	if err != nil {
+		coworkingErr := err.(models.CoworkingErr)
+		c.JSON(coworkingErr.StatusCode, coworkingErr)
+		return
+	}
+	c.JSON(http.StatusOK, rooms)
 }
 
 func GetRoomById(c *gin.Context) {
-	c.String(http.StatusOK, "GetRoomById()")
+	db := c.MustGet("DbKey").(*gorm.DB)
+	room, err := models.GetRoomById(db, c.Param("id"))
+	if err != nil {
+		coworkingErr := err.(models.CoworkingErr)
+		c.JSON(coworkingErr.StatusCode, coworkingErr)
+		return
+	}
+	c.JSON(http.StatusOK, room)
 }
 
 func GetRoomPhotos(c *gin.Context) {
-	c.String(http.StatusOK, "GetRoomPhotos()")
+	db := c.MustGet("DbKey").(*gorm.DB)
+	photos, err := models.GetRoomPhotos(db, c.Param("id"))
+	if err != nil {
+		coworkingErr := err.(models.CoworkingErr)
+		c.JSON(coworkingErr.StatusCode, coworkingErr)
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"id": c.Param("id"), "photos": photos})
 }
